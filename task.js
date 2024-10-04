3745a699db16232fd32f2e82ce5b3453ddc26c0d
programmershakib <programmershakibinfo@gmail.com>
2024-10-04 16:17:40 +0600

    first commit

---
+++ task.js
@@ -0,0 +1,31 @@
+let message = 'Hello!'; 
+setTimeout(() => {
+ message = 'Goodbye!'; 
+}, 0); 
+console.log(message);
+
+
+let x = 1;
+let intervalId = setInterval(() => {
+ console.log(x++);
+ if (x > 3) clearInterval(intervalId);
+}, 1000);
+
+console.log("A");
+Promise.resolve().then(() => console.log("B"));
+setTimeout(() => console.log("C"), 0);
+console.log("D");
+
+try {
+    console.log("Start");
+    throw new Error("Error occurred");
+   } catch (err) {
+    console.log(err.message);
+   } finally {
+    console.log("End");
+   }
+
+   async function myFunction() {
+    return "Hello";
+   }
+   myFunction().then(console.log);
